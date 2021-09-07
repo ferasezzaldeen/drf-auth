2edcb0dd8971688cae6f73624371a26e14fe32e9
Feras Ezzaldeen <ferasezaldeen@gmail.com>
2021-09-07 20:12:34 +0300

    finish lab33

---
+++ food/views.py
@@ -0,0 +1,13 @@
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Dish
+from .serializers import DishSerializer
+# Create your views here.
+
+
+class DishList(ListCreateAPIView):
+    queryset = Dish.objects.all()
+    serializer_class = DishSerializer
+
+class DishDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Dish.objects.all()
+    serializer_class = DishSerializer
